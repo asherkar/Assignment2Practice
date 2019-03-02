58aeeae7e01e283145b29445ac42a69330a53219
asherkar <nsherkar11@gmail.com>
2019-03-02 13:15:04 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,22 @@
+#sample file from https://github.com/avm-ru-cs/cps847-w19/blob/master/ci_play/test.py
+
+import unittest
+
+import my_functions
+
+class TestMyFunc(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_increment_one_1(self):
+        self.assertEqual( my_functions.increment_by_one(1), 2)
+
+    def test_increment_one_2(self):
+        self.assertEqual( my_functions.increment_by_one(0), 1)
+
+    def test_increment_two(self):
+        self.assertEqual( my_functions.increment_by_two(0), 2)
+
+if __name__ == '__main__':
+    unittest.main()
